631f46d4456689f0aad971d08bcde13f8dfd4286
Teja <aravetiteja986@gmail.com>
2024-01-13 17:37:54 +0530

    first commit

---
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -0,0 +1,26 @@
+package cucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
+
+@CucumberOptions(
+        features ="@target/failed_scenarios.txt",
+        glue = "stepDefinitions",
+        monochrome = true,
+        snippets = CAMELCASE,
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        }
+        //dryRun = true
+)
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
+}
---
+++ src/test/java/cucumberOptions/TestNgTestRunner.java
@@ -0,0 +1,28 @@
+package cucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
+
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "stepDefinitions",
+        monochrome = true,
+        snippets = CAMELCASE,
+        tags = "@checkout or @product",
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/failed_scenarios.txt"
+        }
+        //dryRun = true
+)
+public class TestNgTestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
+}
